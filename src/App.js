a4b78f1b26d48f0cc45a8ab272d336f03fb8d4f1
0xEclair <38656355+0xEclair@users.noreply.github.com>
2023-08-06 04:05:00 +0800

    feat: new favicon and search by inscription id

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import { GeistProvider, CssBaseline } from '@geist-ui/core'
-import { Card, Image, Text, Input, Display, Spacer } from '@geist-ui/core'
-import logo from './logo.svg';
+import { Card, Image, Text, Input, Display, Spacer, Code } from '@geist-ui/core'
 import './App.css';
 import { useEffect, useState } from 'react';
 
+const isNumeric = (str) => {
+  if (typeof str != "string") return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
 export const InscriptionDetail = () => {
-  //12321607
-  const fetchInscription = async (number = 0) => {
-    const response = await fetch(`${process.env.REACT_APP_BASE_URL}number=${number}`);
-    const data = await response.json();
-    setInsc(data);
+
+  const [insc, setInsc] = useState({});
+
+  const fetchInscription = async (number) => {
+    if(isNumeric(number)) {
+      const response = await fetch(`${process.env.REACT_APP_BASE_URL}number=${number}`);
+      const data = await response.json();
+      setInsc(data);
+    }
+    else {
+      const response = await fetch(`${process.env.REACT_APP_BASE_URL}id=${number}`);
+      const data = await response.json();
+      setInsc(data);
+    }
+    
   };
+
+  useEffect(() => {
+    fetchInscription("0");
+  }, [])
   const s = `<svg
   xmlns="http://www.w3.org/2000/svg"
   style="background-color: rgb(139, 201, 255);"
@@ -30,10 +49,7 @@ export const InscriptionDetail = () => {
     />
   </g>
 </svg>`;
-  const [insc, setInsc] = useState({});
-  useEffect(() => {
-    fetchInscription();
-  }, [])
+  
 
   return (
     <div className="inscription-detail">
@@ -49,18 +65,22 @@ export const InscriptionDetail = () => {
         <img src={s} alt="" />
         {
           insc?.data?.content_type.startsWith("image/") ?
-            <Card shadow width="400px">
+            <Card hoverable shadow width="400px" height="400px">
               <Display shadow>
                 <Image
-                  src={insc?.data?.content_type !== "image/svg+xml" ? `data:${insc?.data?.content_type};base64, ${insc?.data?.content}` : insc?.data?.content}
-                  width="280px"
-                  height="280px"
-                />
-                {/* <Image src={s} width="280px" height="280px" />
-                <div dangerouslySetInnerHTML={{__html: s}}></div> */}
+                    src={insc?.data?.content_type !== "image/svg+xml" ? `data:${insc?.data?.content_type};base64, ${insc?.data?.content}` : insc?.data?.content}
+                    width="280px"
+                    height="280px"
+                  />
+                  {/* <Image src={s} width="280px" height="280px" />
+                  <div dangerouslySetInnerHTML={{__html: s}}></div> */}
               </Display>
             </Card> :
-            <p>{insc?.data?.content_type}</p>
+            <Card hoverable shadow width="400px" height="400px">
+              <Display shadow>
+                {insc?.data?.content}
+              </Display>
+            </Card>
         }
         {
           <Text>{insc?.data?.content_type}</Text>
